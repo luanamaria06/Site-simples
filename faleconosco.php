cee7a4d194e95ecc7a695a55509eac07a9ba9795
luanamaria06 <159254931+luanamaria06@users.noreply.github.com>
2024-09-24 13:32:24 -0300

    atualização

--- faleconosco.php
+++ faleconosco.php
@@ -1,3 +1,19 @@
 <?php 
 
-    echo "<p> Suporte ao cliente <p>";
+?>
+<div class="formulario">
+    <h1>Fale conosco</h1>
+    <form action="mensagem_receber.php" method="post">
+
+        Nome:<br>
+        <input type="text" name="nome" required><br><br>
+
+        E-mail:<br>
+        <input type="email" name="email" required><br><br>
+
+        Mensagem:<br>
+        <textarea name="mensagem" value="mensagem"></textarea><br><br>
+        
+        <input type="submit" value="Enviar">
+    </form>
+</div>
--- galeria.php
+++ galeria.php
@@ -1,34 +1,34 @@
 <?php
 ?>
 <div class="artes1">
-    <section>
+    <section >
         <img src="imagens/artedelilo&stitch01.jpg" style="width: 180px; height: 190px;">
-        <p>Arte 01</p>
+        <p class="legenda_galeria">Arte 01</p>
     </section>
 
     <section>
         <img src="imagens/artedelilo&stitch02.jpg" style="width: 180px; height: 190px;">
-        <p>Arte 02</p>
+        <p class="legenda_galeria">Arte 02</p>
     </section>
 
-    <section>
+    <section >
         <img src="imagens/artedelilo&stitch03.jpg" style="width: 180px; height: 190px;">
-        <p>Arte 03</p>
+        <p class="legenda_galeria">Arte 03</p>
     </section>
 
     <section>
         <img src="imagens/artedelilo&stitch04.jpg" style="width: 180px; height: 190px;">
-        <p>Arte 04</p>
+        <p class="legenda_galeria">Arte 04</p>
     </section> 
 
     <section>
         <img src="imagens/artedelilo&stitch05.jpg" style="width: 180px; height: 190px;">
-        <p>Arte 05</p>
+        <p class="legenda_galeria">Arte 05</p>
     </section>
     
     <section>
         <img src="imagens/artedelilo&stitch06.jpg" style="width: 180px; height: 190px;">
-        <p>Arte 06</p>
+        <p class="legenda_galeria">Arte 06</p>
     </section>
 
 </div>
@@ -36,32 +36,32 @@
 
     <section>
         <img src="imagens/artedelilo&stitch07.jpg" style="width: 180px; height: 190px;">
-        <p>Arte 07</p>
+        <p class="legenda_galeria">Arte 07</p>
     </section>
 
     <section>
         <img src="imagens/artedelilo&stitch08.jpg" style="width: 180px; height: 190px;">
-        <p>Arte 08</p>
+        <p class="legenda_galeria">Arte 08</p>
     </section>
 
     <section>
         <img src="imagens/artedelilo&stitch09.jpg" style="width: 180px; height: 190px;">
-        <p>Arte 09</p>
+        <p class="legenda_galeria">Arte 09</p>
     </section>
 
     <section>
         <img src="imagens/artedelilo&stitch10.jpg" style="width: 180px; height: 190px;">
-        <p>Arte 10</p>
+        <p class="legenda_galeria">Arte 10</p>
     </section> 
 
     <section>
         <img src="imagens/artedelilo&stitch11.jpg" style="width: 180px; height: 190px;">
-        <p>Arte 11</p>
+        <p class="legenda_galeria">Arte 11</p>
     </section>
 
     
     <section>
         <img src="imagens/artedelilo&stitch12.jpg" style="width: 180px; height: 190px;">
-        <p>Arte 12</p>
+        <p class="legenda_galeria">Arte 12</p>
     </section>
 </div>
