5982f8edfb49cc0d7ae0b4a74c80b8ab8fed0baf
luanamaria06 <159254931+luanamaria06@users.noreply.github.com>
2024-09-29 11:18:49 -0300

    atualização

---
+++ personagens.php
@@ -0,0 +1,21 @@
+<?php 
+?>
+
+<div class="personagem1">
+    <section >
+        <img aling="left" src="imagens/personagem_lilo.jpeg" style="width: 250px; height:auto;">
+    </section>
+
+    <section>
+        <h2>Lilo Pelekai</h2>
+        
+        <p>
+        Lilo é uma garotinha havaiana de 6 anos, cheia de imaginação e curiosidade.
+        Ela adora tirar fotos de pessoas e situações estranhas, acredita em extraterrestres 
+        e é apaixonada por tudo o que é diferente. Lilo tem uma personalidade forte, é teimosa, 
+        mas também doce e sensível, especialmente depois de perder seus pais em um acidente. 
+        Ela vive com sua irmã mais velha, Nani, e luta para se encaixar entre as outras crianças. 
+        Sua amizade com Stitch é única e transformadora para ambos. 
+        </p>
+    </section>
+</div>
