efd69a026daf8f46d009496eb2a71ec95abdaaa9
luanamaria06 <159254931+luanamaria06@users.noreply.github.com>
2024-09-29 11:54:56 -0300

    atualização

--- personagens.php
+++ personagens.php
@@ -1,7 +1,6 @@
 <?php 
 ?>
-
-<div class="personagens">
+<div class="filmes">
     <section >
         <img aling="left" src="imagens/personagem_lilo.jpeg" style="width: 250px; height:auto;">
     </section>
@@ -10,12 +9,173 @@
         <h2>Lilo Pelekai</h2>
         
         <p>
-        Lilo é uma garotinha havaiana de 6 anos, cheia de imaginação e curiosidade.
-        Ela adora tirar fotos de pessoas e situações estranhas, acredita em extraterrestres 
-        e é apaixonada por tudo o que é diferente. Lilo tem uma personalidade forte, é teimosa, 
-        mas também doce e sensível, especialmente depois de perder seus pais em um acidente. 
-        Ela vive com sua irmã mais velha, Nani, e luta para se encaixar entre as outras crianças. 
-        Sua amizade com Stitch é única e transformadora para ambos. 
+            Lilo é uma garotinha havaiana de 6 anos, cheia de imaginação e curiosidade.
+            Ela adora tirar fotos de pessoas e situações estranhas, acredita em extraterrestres 
+            e é apaixonada por tudo o que é diferente. Lilo tem uma personalidade forte, é teimosa, 
+            mas também doce e sensível, especialmente depois de perder seus pais em um acidente. 
+            Ela vive com sua irmã mais velha, Nani, e luta para se encaixar entre as outras crianças. 
+            Sua amizade com Stitch é única e transformadora para ambos. 
         </p>
     </section>
 </div>
+
+<div class="filmes">
+    <section >
+        <img aling="left" src="imagens/personagem_stitch.jpeg" style="width: 250px; height:auto;">
+    </section>
+
+    <section>
+        <h2>Stitch (Experiência 626)</h2>
+        
+        <p>
+            Stitch é um experimento genético criado pelo cientista alienígena Dr. Jumba Jookiba.
+            Sua programação inicial é causar caos e destruição, mas depois de conhecer Lilo, 
+            ele começa a aprender sobre família e compaixão. Apesar de sua aparência adorável, 
+            Stitch é superforte, inteligente e quase indestrutível. Ao longo do filme, ele evolui 
+            de uma criatura caótica para um membro leal da "ohana" de Lilo.
+        </p>
+    </section>
+</div>
+
+<div class="filmes">
+    <section >
+        <img aling="left" src="imagens/personagem_nani.png" style="width: 250px; height:auto;">
+    </section>
+
+    <section>
+        <h2>Nani Pelekai</h2>
+        
+        <p>
+            Nani é a irmã mais velha de Lilo e sua tutora legal. 
+            Após a morte dos pais, ela assume a responsabilidade de cuidar de Lilo. 
+            Nani é uma personagem resiliente, equilibrando seu trabalho, sua vida 
+            pessoal e o desafio de criar sua irmã mais nova. Ela é amorosa, embora 
+            muitas vezes sobrecarregada, e faz de tudo para manter Lilo e sua família unida.
+        </p>
+    </section>
+</div>
+
+<div class="filmes">
+    <section >
+        <img aling="left" src="imagens/personagem_Jumba Jookiba.png" style="width: 250px; height:auto;">
+    </section>
+
+    <section>
+        <h2>Dr. Jumba Jookiba</h2>
+        
+        <p>
+            Jumba é o criador de Stitch e um cientista alienígena um tanto excêntrico. 
+            No início, ele é enviado à Terra para capturar Stitch, mas acaba se aliando 
+            a Lilo e sua família. Embora tenha sido inicialmente um vilão, Jumba se mostra 
+            afetuoso e divertido, desenvolvendo uma relação peculiar com os outros personagens.
+        </p>
+    </section>
+</div>
+
+<div class="filmes">
+    <section >
+        <img aling="left" src="imagens/personagem_pleakley.png" style="width: 250px; height:auto;">
+    </section>
+
+    <section>
+        <h2>Agente Wendy Pleakley</h2>
+        
+        <p>
+            Pleakley é um agente da Federação Galáctica e parceiro de Jumba na missão de capturar Stitch. 
+            Ele é obcecado por preservar a Terra e suas formas de vida, e tem uma personalidade peculiar
+            e neurótica. Pleakley, com sua aparência fina e de um olho só, é cômico e muitas vezes inseguro.
+            Ele acaba se afeiçoando aos costumes humanos e à vida na Terra.
+        </p>
+    </section>
+</div>
+
+<div class="filmes">
+    <section >
+        <img aling="left" src="imagens/perosnagem_cobra bubbles.png" style="width: 250px; height:auto;">
+    </section>
+
+    <section>
+        <h2>Cobra Bubbles</h2>
+        
+        <p>
+            Cobra Bubbles é um ex-agente da CIA que agora trabalha como assistente social 
+            e está encarregado de avaliar se Nani é capaz de cuidar de Lilo. Apesar de seu 
+            exterior intimidador, Bubbles se preocupa genuinamente com o bem-estar de Lilo e sua família. 
+            Ele tem uma história misteriosa e é revelado que ele já teve envolvimento com alienígenas.
+        </p>
+    </section>
+</div>
+
+<div class="filmes">
+    <section >
+        <img aling="left" src="imagens/personagem_David.png" style="width: 250px; height:auto;">
+    </section>
+
+    <section>
+        <h2>David Kawena</h2>
+        
+        <p>
+            David é amigo de Nani e tem uma queda por ela. 
+            Ele é surfista e trabalha em um luau. David é gentil, 
+            sempre apoiando Nani e Lilo, e tenta ajudar da melhor maneira possível, 
+            mesmo nas situações mais difíceis. Ele é um personagem que traz leveza e romance para a história.
+        </p>
+    </section>
+</div>
+
+<div class="filmes">
+    <section >
+        <img aling="left" src="imagens/personagem_mertle edmonds.png" style="width: 250px; height:auto;">
+    </section>
+
+    <section>
+        <h2>Mertle Edmonds</h2>
+        
+        <p>
+            Mertle é a principal antagonista de Lilo na escola. 
+            Ela é uma garota mimada e líder de um grupo de amigas que 
+            frequentemente provocam e zombam de Lilo por ser diferente. 
+            Mertle despreza Stitch e se recusa a ser amiga de Lilo, 
+            tornando-se uma rival constante. Apesar de sua atitude mesquinha, 
+            Mertle representa os desafios que Lilo enfrenta ao tentar se encaixar e lidar com a rejeição social.
+        </p>
+    </section>
+</div>
+
+<div class="filmes">
+    <section >
+        <img aling="left" src="imagens/personagem_gantu.png" style="width: 250px; height:auto;">
+    </section>
+
+    <section>
+        <h2>Capitão Gantu</h2>
+        
+        <p>
+            Gantu é um imponente alienígena e ex-capitão da Federação Galáctica. 
+            Ele é enviado à Terra para capturar Stitch, mas frequentemente fracassa em sua missão. 
+            Gantu é extremamente disciplinado, mas seu tamanho enorme e personalidade rígida o 
+            fazem parecer desajeitado nas situações terrestres. Embora inicialmente pareça um vilão, 
+            ele está apenas cumprindo seu dever, o que o coloca em oposição aos heróis da história.
+        </p>
+    </section>
+</div>
+
+<div class="filmes">
+    <section >
+        <img aling="left" src="imagens/personagem_grand councilwoman.png" style="width: 250px; height:auto;">
+    </section>
+
+    <section>
+        <h2>Grand Councilwoman</h2>
+        
+        <p>
+            A Grand Councilwoman é a líder suprema da Federação Galáctica. 
+            Ela é quem condena Stitch à prisão inicialmente e depois ordena 
+            sua captura quando ele escapa para a Terra. Ela é justa e sensata, 
+            mas também severa quando necessário. Apesar de sua posição de autoridade, 
+            a Grand Councilwoman demonstra compaixão, especialmente no final, quando 
+            reconhece o vínculo entre Lilo e Stitch e concede a eles uma chance de viver juntos em paz.
+        </p>
+    </section>
+</div>
+
