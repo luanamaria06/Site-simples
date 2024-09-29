83e467d9f541ac58057a8503249c91c5a659daf1
luanamaria06 <159254931+luanamaria06@users.noreply.github.com>
2024-09-29 14:07:11 -0300

    atualização

--- quemsomos.php
+++ quemsomos.php
@@ -1,3 +1,45 @@
 <?php 
+?>
 
-    echo "<p> Quem somos <p>";
+<div class="quemsomos">
+    <h2>
+        Quem Somos?
+    </h2>
+
+    <ul>
+        <li>Bem-vindo ao nosso site dedicado ao incrível mundo de Lilo & Stitch! 
+            Somos apaixonados por essa história que cativou tantas pessoas ao redor 
+            do mundo e criamos este espaço para compartilhar essa paixão com outros fãs.
+        </li>
+    </ul>
+
+    <ul>
+        <li>Nosso site foi desenvolvido com o objetivo de reunir tudo o que amamos sobre Lilo & Stitch
+             em um só lugar. Aqui, você vai encontrar informações sobre os dois filmes da série,
+             com resumo.
+        </li>
+    </ul>
+
+    <ul>
+        <li>Além disso, dedicamos uma parte especial para os personagens, onde apresentamos 
+            desde a amada Lilo e o adorável Stitch até os outros personagens que 
+            tornam esse universo tão divertido e diversificado. Para quem quer saber mais sobre 
+            suas histórias e características, esse é o lugar perfeito.
+        </li>
+    </ul>
+
+    <ul>
+        <li>Também temos um espaço para artes exclusivas, onde celebramos a beleza visual da série. 
+            Nossa galeria exibe concept arts, esboços e ilustrações que mostram o processo criativo 
+            por trás desse universo tão vibrante.
+        </li>
+    </ul>
+
+    <ul>
+        <li>Nosso intuito é oferecer um ponto de encontro para fãs de todas as idades,
+             onde possam reviver as aventuras, aprender mais sobre os personagens e se 
+             encantar com a magia que Lilo & Stitch traz.
+        </li>
+    </ul>
+
+</div>
