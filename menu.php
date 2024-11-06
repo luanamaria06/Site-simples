a266192dceec963a61aca3bfa16fb03f3ab49569
luanamaria06 <159254931+luanamaria06@users.noreply.github.com>
2024-11-06 12:54:47 -0300

    atualização

--- menu.php
+++ menu.php
@@ -1,2 +1,15 @@
 <?php
 ?>
+
+<div>
+    <header>
+            <nav>
+                <ul>
+                    <li><a href="?pg=principal">Bem-vindos</a></li>
+                    <li><a href="?pg=filmes">Filmes</a></li>
+                    <li><a href="?pg=personagens">Personagens</a></li>
+                    <li><a href="?pg=galeria">Galeria</a></li>
+                </ul>
+            </nav>
+    </header>
+<div>
--- topo.php
+++ topo.php
@@ -5,14 +5,6 @@
 <div>
         <header>
             <h1>Mundo de Lilo & Stitch</h1>
-            <nav>
-                <ul>
-                    <li><a href="?pg=principal">Bem-vindos</a></li>
-                    <li><a href="?pg=filmes">Filmes</a></li>
-                    <li><a href="?pg=personagens">Personagens</a></li>
-                    <li><a href="?pg=galeria">Galeria</a></li>
-                </ul>
-            </nav>
         </header>
 </div>
 
